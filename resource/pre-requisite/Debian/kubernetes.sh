fd668fcf5337e507f649d9b4a4381bd237c3275e
Sodo So <support@sodo.so>
2024-03-13 17:08:53 +0800

    fix k8s pre-requisite

--- resource/pre-requisite/Debian/kubernetes.sh
+++ resource/pre-requisite/Debian/kubernetes.sh
@@ -9,9 +9,9 @@ export PATH
 SHORT_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
 SHORT_VERSION=${SHORT_VERSION%.*}
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$SHORT_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
 curl -fsSL https://pkgs.k8s.io/core:/stable:/$SHORT_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$SHORT_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # sudo mkdir -m 755 /etc/apt/keyrings
 # curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
